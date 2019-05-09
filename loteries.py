95b1c8a2e420501b7d3573f16b9b8a7309d2821a
SDS <sebastien.delolme-sabatier@etu.univ-lyon1.fr>
2019-05-09 22:58:59 +0200

    code factorization

--- loteries.py
+++ loteries.py
@@ -8,7 +8,7 @@ import sys
 nbboules = 0
 first_col = 0
 nbboules_c = 0
-
+frequencies = []
 def get_arg():
 	arg_l=[]
 	for arg in sys.argv: 
@@ -37,6 +37,31 @@ def input_file():
 			return(args[2]+" is not a correct argument : use 'loto' or 'euromillions' instead")
 	else:
 		return(args[1]+" is not a correct argument, use -i or --input to choose the file to convert")
+
+def proba(lines,col_dic,f_col,nb_b):
+	for i in lines:
+		cells = i.split(';')
+		col_dic[int(cells[f_col])] = col_dic[int(cells[f_col])] + 1
+	for k in col_dic.keys():
+		col_dic[k] = col_dic[k]/tot_tirages
+	return(col_dic)
+
+
+		# if col_dic[k] < 1/nbboules :                                 
+		# 	dmin_b1[k] = dico_col1[k]
+
+def number_choice(col_dic,nb_b,tir):
+	dmin = {}
+	for k in col_dic.keys():
+		if col_dic[k]<1/nb_b :
+			dmin[k] = col_dic[k]
+	b = random.choice(list(dmin.keys()))
+	while(b in tir):
+		b = random.choice(list(dmin.keys()))
+	return(b)
+
+
+	
 
 #################################
 ########  Main program  #########
@@ -90,22 +115,13 @@ for i in l2:
 # le moins                                                #
 ###########################################################
 
-dmin_b1 = {}
-
-for i in lines :
- 	cells = i.split(';')
- 	dico_col1[int(cells[first_col])] = dico_col1[int(cells[first_col])] + 1 
-
-for k in dico_col1.keys():
-	dico_col1[k] = dico_col1[k]/tot_tirages
-	if dico_col1[k] < 1/nbboules :                                 
-		dmin_b1[k] = dico_col1[k]
+dico_col1=proba(lines,dico_col1,first_col,nbboules)
 
 ##########################################
 # première boule : choix du nombre       # 
 ##########################################
 
-boule_1 = random.choice(list(dmin_b1.keys()))
+boule_1 = number_choice(dico_col1,nbboules,tirage)
 tirage.append(boule_1)
 
 ##################
@@ -116,53 +132,26 @@ tirage.append(boule_1)
 # deuxième boule : traitement du tableau # 
 ##########################################
 
-dmin_b2 = {}
-
-for i in lines :
-	cells = i.split(';')
-	if int(cells[first_col+1]) not in tirage:
-		dico_col2[int(cells[first_col+1])] = dico_col2[int(cells[first_col+1])] + 1
-	else:
-		dico_col2[int(cells[first_col+1])] = "tirée"
-
-for k in dico_col2.keys():
-	if dico_col2[k]!="tirée":
-		dico_col2[k] = dico_col2[k]/tot_tirages
-		if dico_col2[k] < 1/(nbboules-1) :
-			dmin_b2[k] = dico_col2[k]
-
+dico_col2 = proba(lines,dico_col2,first_col+1,nbboules-1)
 
 ##########################################
 # deuxième boule : choix du nombre       # 
 ##########################################
 
-boule_2 = random.choice(list(dmin_b2.keys()))
+boule_2 = number_choice(dico_col2,nbboules-1,tirage)
 tirage.append(boule_2)
 
 ###################
 # troisième boule # 
 ###################
 
-dmin_b3 = {}
-
-for i in lines :
-	cells = i.split(';')
-	if int(cells[first_col+2]) in tirage:
-		dico_col3[int(cells[first_col+2])] = "tirée"
-	else:
-		dico_col3[int(cells[first_col+2])] = dico_col3[int(cells[first_col+2])] + 1
-
-for k in dico_col3.keys():
-	if dico_col3[k]!="tirée":
-		dico_col3[k] = dico_col3[k]/tot_tirages
-		if dico_col3[k] < 1/(nbboules-2) :
-			dmin_b3[k] = dico_col3[k]
+dico_col3=proba(lines,dico_col3,first_col+2,nbboules-2)
 
 ##########################################
 # troisème boule : choix du nombre       # 
 ##########################################
 
-boule_3 = random.choice(list(dmin_b3.keys()))
+boule_3 = number_choice(dico_col3,nbboules-2,tirage)
 tirage.append(boule_3)
 
 ###################
@@ -170,51 +159,26 @@ tirage.append(boule_3)
 ###################
 
 dmin_b4 = {}
-
-for i in lines :
-	cells = i.split(';')
-	if int(cells[first_col+3]) in tirage:
-		dico_col4[int(cells[first_col+3])] = "tirée"
-	else:
-		dico_col4[int(cells[first_col+3])] = dico_col4[int(cells[first_col+3])] + 1
-
-for k in dico_col4.keys():
-	if dico_col4[k]!="tirée":
-		dico_col4[k] = dico_col4[k]/tot_tirages
-		if dico_col4[k] < 1/(nbboules-3) :
-			dmin_b4[k] = dico_col4[k]
+dico_col4=proba(lines,dico_col4,first_col+3,nbboules-3)
 
 # ##########################################
 # # quatrième boule : choix du nombre      # 
 # ##########################################
 
-boule_4 = random.choice(list(dmin_b4.keys()))
+boule_4 = number_choice(dico_col4,nbboules-3,tirage)
 tirage.append(boule_4)
 
 ###################
 # cinquième boule # 
 ###################
 
-dmin_b5 = {}
-
-for i in lines :
-	cells = i.split(';')
-	if int(cells[first_col+4]) in tirage:
-		dico_col5[int(cells[first_col+4])] = "tirée"
-	else:
-		dico_col5[int(cells[first_col+4])] = dico_col5[int(cells[first_col+4])] + 1
-
-for k in dico_col5.keys():
-	if dico_col5[k]!="tirée":
-		dico_col5[k] = dico_col5[k]/tot_tirages
-		if dico_col5[k] < 1/(nbboules-4) :
-			dmin_b5[k] = dico_col5[k]
+dico_col5=proba(lines,dico_col5,first_col+4,nbboules-4)
 
 ##########################################
 # cinquième boule : choix du nombre      # 
 ##########################################
 
-boule_5 = random.choice(list(dmin_b5.keys()))
+boule_5 = number_choice(dico_col5,nbboules-4,tirage)
 tirage.append(boule_5)
 
 
@@ -222,22 +186,13 @@ tirage.append(boule_5)
 # Première étoile   # 
 #####################
 
-dmin_e1 = {}
-
-for i in lines :
-	cells = i.split(';')
-	dico_cole1[int(cells[first_col+5])] = dico_cole1[int(cells[first_col+5])] + 1
-
-for k in dico_cole1.keys():
-	dico_cole1[k] = dico_cole1[k]/tot_tirages
-	if dico_cole1[k] < 1/nbboules_c :
-		dmin_e1[k] = dico_cole1[k]
+dico_cole1 = proba(lines,dico_cole1,first_col+5,nbboules_c)
 
 ##########################################
 # première étoile : choix du nombre      # 
 ##########################################
 
-boule_e1 = random.choice(list(dmin_e1.keys()))
+boule_e1 = number_choice(dico_cole1,nbboules_c,tirage_e)
 tirage_e.append(boule_e1)
 
 #####################
@@ -248,24 +203,13 @@ tirage_e.append(boule_e1)
 euOrlo = get_arg()
 
 if euOrlo[2] == "euromillions":
-	dmin_e2 = {}
+	dico_cole2 = proba(lines,dico_cole2,first_col+6,nbboules_c-1)
 
-	for i in lines :
-		cells = i.split(';')
-		if int(cells[first_col+6]) in tirage_e:
-			dico_cole2[int(cells[first_col+6])] = "tirée"
-		else:
-			dico_cole2[int(cells[first_col+6])] = dico_cole2[int(cells[first_col+6])] + 1
-	for k in dico_cole2.keys():
-		if dico_cole2[k]!="tirée":
-			dico_cole2[k] = dico_cole2[k]/tot_tirages
-			if dico_cole2[k] < 1/(nbboules_c-1) :
-				dmin_e2[k] = dico_cole2[k]
 ##########################################
 # deuxième étoile : choix du nombre      # 
 ##########################################
 
-	boule_e2 = random.choice(list(dmin_e2.keys()))
+	boule_e2 = number_choice(dico_cole2,nbboules_c-1,tirage_e)
 	tirage_e.append(boule_e2)
 
 
@@ -282,18 +226,72 @@ elif euOrlo[2] == "loto":
 	print("numéro chance : "+str(tirage_e[0]))
 
 
+
 #########################################
 # Vérification de l'hypothèse de départ # 
 #########################################
 
-print(euOrlo)
-if len(euOrlo)>3:
-	if euOrlo[3] == "-opt":
-		if len(euOrlo)>4 and euOrlo[4] == "analyse":
-			print("première boule : ")
-			res_1 = input()
-			print("deuxième boule : ")
-			res_2 = input()
-			print("troisème boule : ")
-			res_3 = input()
 
+# if len(euOrlo)>3:
+# 	if euOrlo[3] == "-opt":
+# 		if len(euOrlo)>4 and euOrlo[4] == "analyse":
+#  			print("première boule : ")
+#  			res_1 = input()
+#  			print("deuxième boule : ")
+#  			res_2 = input()
+#  			print("troisème boule : ")
+#  			res_3 = input()
+#  			print("quatrième boule : ")
+#  			res_4 = input()
+#  			print("cinquième boule : ")
+#  			res_5 = input()
+#  			tirage_vrai = []
+#  			if euOrlo[2] == "euromillions":
+#  				frequencies.extend((dico_col1,dico_col2,dico_col3,dico_col4,dico_col5,dico_cole1,dico_cole2))
+#  				print("première étoile : ")
+#  				eto_1 = input()
+#  				print("deuxième étoile : ")
+#  				eto_2 = input()
+#  				tirage_vrai.extend((res_1,res_2,res_3,res_4,res_5,eto_1,eto_2))
+#  				print(tirage_vrai)
+#  				i=0
+#  				while i<len(tirage_vrai):
+#  					if(frequencies[i][int(tirage_vrai[i])]>1/(nbboules-i)):							
+#  						print(tirage_vrai[i] + " fait partie de ceux qui sortent le plus")
+#  					else:
+#  						print(tirage_vrai[i] + " fait partie de ceux qui sortent le moins")
+#  					i = i+1
+#  			elif euOrlo[2] == "loto":
+#  				frequencies.extend((dico_col1,dico_col2,dico_col3,dico_col4,dico_col5,dico_cole1))
+#  				print("numéro chance : ")
+#  				num_cha = input()
+#  				tirage_vrai.extend((res_1,res_2,res_3,res_4,res_5,num_cha))
+#  				print(tirage_vrai)
+#  				while i<len(tirage_vrai):
+#  					if(frequencies[i][int(tirage_vrai[i])]>1/(nbboules-i)):							
+#  						print(tirage_vrai[i] + " fait partie de ceux qui sortent le plus")
+#  					else:
+#  						print(tirage_vrai[i] + " fait partie de ceux qui sortent le moins")
+#  					i = i+1				
+
+
+##############
+##   To do  ##
+##############
+
+
+'''
+
+- readme
+- Tout en anglais
+- Ne pas faire de tirage quand en mode analyse
+- Rajouter les plus
+- Création d'un fichier d'analyse :
+	- info sur la proba du numéro qui est sorti
+	- nombre de fois qu'un numéro sorti à fait partie des plus ou des moins
+	- 
+
+
+
+
+'''
